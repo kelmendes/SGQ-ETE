5648f274f0915cba4ae1e6d72aa4571a8741e6b3
klebsonm <klerbersonmendes@gmail.com>
2018-03-02 23:22:11 -0300

    Login
    configurações de login

---
+++ function/configdb.php
@@ -0,0 +1,16 @@
+<?php
+	
+	/**
+	* CLASS PARA MANTER OS DADOS IMPORTANTES DO BANCO DE DADOS
+	*/
+	class Configdb
+	{
+		// ATRIBUTOS DA CLASS PARA CONEXÃO 
+		var $host = 'localhost';
+		var $dbname = 'ete_pi1';
+		var $user = 'root';
+		var $password = '';
+	}
+
+
+?>
