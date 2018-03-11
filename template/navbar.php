3e4ebf04b436205c0a32cec16a6838e367a49117
klebsonm <klerbersonmendes@gmail.com>
2018-03-11 16:08:40 -0300

    logout navBar
    adicionando o link de logout na navBar

--- template/navbar.php
+++ template/navbar.php
@@ -3,17 +3,25 @@
             <div class="container">
                 <a class="navbar-brand" href="#">ETE Palmares</a>
                 <ul class="nav navbar-nav navbar-right">
-                  <li><a href="./home">Home</a></li>
-                  <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">User<span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                      <li><a href="#">Profile</a></li>
-                      <li><a href="#">Mensagens</a></li>
-                      <li><a href="#">Questões cadastradas</a></li>
-                      <li role="separator" class="divider"></li>
-                      <li><a href="#">Logout</a></li>
-                    </ul> 
-                  </li> 
+                    <li><a href="./home">Home</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">User<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="#">Profile</a>
+                            </li>
+                            <li>
+                                <a href="#">Mensagens</a>
+                            </li>
+                            <li>
+                                <a href="#">Questões cadastradas</a>
+                            </li>
+                            <li role="separator" class="divider"></li>
+                            <li>
+                                <a href="./function/logout">Logout</a>
+                            </li>
+                        </ul> 
+                    </li> 
                 </ul>         
             </div>
             <!-- end container -->
