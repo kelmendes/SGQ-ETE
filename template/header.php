bc334b39fef835785d2e697ccf5c729dfdb80105
Klebson Mendes <mendes.klebson@gmail.com>
2018-04-27 14:42:13 -0300

    espaçamento

--- template/header.php
+++ template/header.php
@@ -1,6 +1,9 @@
     <!-- meta tags -->
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+        
         <!-- COR Barra Mobile -->
         <!-- Chrome, Firefox OS, Opera and Vivaldi -->
         <meta name="theme-color" content="##000080">
@@ -8,6 +11,9 @@
         <meta name="msapplication-navbutton-color" content="##000080">
         <!-- iOS Safari -->
         <meta name="apple-mobile-web-app-status-bar-style" content="##000080">
+
+
+
         <link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
         <link rel="stylesheet" type="text/css" href="./css/bootstrap-theme.css">
         <style>
