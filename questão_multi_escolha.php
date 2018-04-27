39f38ca7b08c3a51521b4d10ee1dddbcd79617d4
Klebson Mendes <mendes.klebson@gmail.com>
2018-04-27 16:19:47 -0300

    aplicando template base

---
+++ questão_multi_escolha.php
@@ -0,0 +1,29 @@
+<?php
+    // VERIFICAR SE O USUARIO ESTA AUTENTICADO 
+    require './function/verificar_login.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <!-- ADICIONANDO HEADER PADRÃO -->
+        <?php include './template/header.php'; ?>
+
+    </head>
+    <body>
+        <!-- ADICIONANDO A NAV BAR -->
+        <?php include './template/navbar.php'; ?>
+        
+        <div class="container">
+             <div class="row" >
+        
+            </div>
+            <!-- end  row --> 
+        </div>
+        <!--edn container  -->
+
+
+    <!-- ADICIONANDO HEADER PADRÃO -->
+    <?php include './template/footer.php'; ?>
+    </body>
+</html>
