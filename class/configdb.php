afc8736a30d7dbd4f0d4d5d15abaf67ce54ce880
Klebson Mendes <mendes.klebson@gmail.com>
2018-05-02 10:26:10 -0300

    new db

--- class/configdb.php
+++ class/configdb.php
@@ -10,5 +10,12 @@
 		var $dbname = 'test';
 		var $user = 'root';
 		var $password = '';
+
+
+		// ATRIBUTOS DA CLASS PARA CONEXÃO 
+		/* var $host = 'testep1.mysql.dbaas.com.br';
+		var $dbname = 'testep1';
+		var $user = 'testep1';
+		var $password = 'P@ssAlun0'; */
 	}
 ?>
