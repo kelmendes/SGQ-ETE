9affe3c6fafaa55a922afc25befa68dc137e2f16
klebsonm <klerbersonmendes@gmail.com>
2018-05-07 17:15:01 -0300

    change db

--- template/panel_sidebar_questoes_selecionadas.php
+++ template/panel_sidebar_questoes_selecionadas.php
@@ -17,7 +17,7 @@
                                     $user = 'root';
                                     $password = '';
 
-                                    $conn = new PDO('mysql:host=localhost;dbname=p1teste', $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+                                    $conn = new PDO('mysql:host=localhost;dbname=teste2', $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
 
                                     
